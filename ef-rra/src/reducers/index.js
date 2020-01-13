cd2b7594f4eb4ffeb5efc49b6eb69267b1591299
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-13 13:54:44 -0800

    updated reducers/index.js -- imported F_D_Start,Success, Fail -- set up the case for each

--- ef-rra/src/reducers/index.js
+++ ef-rra/src/reducers/index.js
@@ -1,12 +1,38 @@
+import {
+    FETCH_DOGS_START,
+    FETCH_DOGS_SUCCESS,
+    FETCH_DOGS_FAIL
+} from '../actions'
 
 const initialState = {
-    title: 'From Reducer!',
-    name: 'Fitz'
+    dogs: [],
+    error: '',
+    isFetching: false
 }
 
 const reducer = (state = initialState, action) => {
     console.log(state)
+
     switch(action.type) {
+        case FETCH_DOGS_START:
+            return {
+                ...state,
+                isFetching: true,
+                error: ''
+            };
+        case FETCH_DOGS_SUCCESS:
+            return {
+                ...state,
+                dogs: action.payload,
+                isFetching: false,
+                error: ''
+            }
+        case FETCH_DOGS_FAIL:
+            return {
+                ...state,
+                error: ''
+            }
+        
         default:
             return state;
     }
